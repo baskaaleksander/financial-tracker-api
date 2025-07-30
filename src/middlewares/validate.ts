ddb998e6e930eb3728dcb67bcefb571532554ffb
Aleksander Baska <baskaaleksander03@gmail.com>
2025-07-30 09:01:22 +0200

    feat: add Zod validation middleware, user model, and update package dependencies

---
+++ src/middlewares/validate.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express';
+import { z } from 'zod';
+
+export const validate =
+  (schema: z.Schema) => (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+
+    if (!result.success) {
+      return res.status(400).json({
+        error: result.error.flatten(),
+      });
+    }
+    req.body = result.data;
+    next();
+  };
